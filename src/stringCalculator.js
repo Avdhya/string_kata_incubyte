077ba081743a6d5c82c12fc7a6523b786be1bd73
Avadhut <naikwadiavadhut0110@gmail.com>
2024-12-09 17:01:38 +0530

    Initial implementation of add function: handles empty string && added run command in package.json

---
+++ src/stringCalculator.js
@@ -0,0 +1,10 @@
+function add(numbers) {
+    if (numbers == '') {
+        console.log("Input is an empty string, returning 0");
+        return 0;
+    }
+}
+
+module.exports = add;
+
+console.log(add(""))
