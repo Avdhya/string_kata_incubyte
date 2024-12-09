f5a4162f2ca030c14992b2cb91f0df7d24640001
Avadhut <naikwadiavadhut0110@gmail.com>
2024-12-09 17:15:29 +0530

    Handle single number input as string

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,10 +1,15 @@
 function add(numbers) {
-    if (numbers == '') {
+    if (numbers === "") {
         console.log("Input is an empty string, returning 0");
         return 0;
     }
+
+    let num = parseInt(numbers);
+    console.log(`Single number ${numbers}, returning ${num}`);
+    return num;
 }
 
+// Export the function to make it available for imports in other files
 module.exports = add;
 
-console.log(add(""))
+console.log(add("1"))
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,5 +1,9 @@
-const { add } = require('../src/stringCalculator');
+const add = require('../src/stringCalculator'); 
 
 test('should return 0 for an empty string', () => {
-  expect(add('')).toBe(0);
+    expect(add('')).toBe(0);
+});
+
+test('should return the number itself for a single number string', () => {
+    expect(add("1")).toBe(1);
 });
